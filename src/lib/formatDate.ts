3d08a81a27ff3a68a833d8f16d5fd0bcc51bc3ca
sionyeom <duatldhs5555@gmail.com>
2025-04-04 15:43:47 +0900

    feat:기능작성

--- src/lib/formatDate.ts
+++ src/lib/formatDate.ts
@@ -1,9 +1,11 @@
+import moment from "moment";
+
 export const formatDate = (date: Date) => {
-  return date?.toISOString()?.split("T")[0]; // YYYY-MM-DD 형식으로 변환
+  return moment(date).format("YYYY-MM-DD"); // YYYY-MM-DD 형식으로 변환
 };
 
 export const formatTime = (date: Date) => {
-  return date?.toTimeString()?.split(" ")[0]; // HH:MM:SS 형식으로 변환
+  return moment(date).format("HH:mm"); // HH:MM:SS 형식으로 변환
 };
 
 export const formatStringDate = (stringDate: string | Date) => {
